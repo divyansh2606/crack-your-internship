63b264eebca0e99b86a8e22f03fbd8a205aebfa8
divyansh2606 <111186936+divyansh2606@users.noreply.github.com>
2024-02-08 22:28:04 +0530

    Time: 21 ms (32.93%), Space: 21.7 MB (12.34%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int i=0;
+        for(int j=0;j<arr.size();j++)
+        {
+            if(arr[j]!=0)
+            {
+                swap(arr[j],arr[i]);
+                i++;
+            }
+        }
+        
+     }
+};
