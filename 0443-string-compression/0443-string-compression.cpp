07c763d0fde26fba9c4ce41b5346cb3f7cff229f
divyansh2606 <111186936+divyansh2606@users.noreply.github.com>
2024-02-28 18:29:00 +0530

    Time: 3 ms (80.27%), Space: 12.6 MB (19.01%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        
+         int i=0;
+        int ansindex=0;
+        int n=chars.size();
+        while(i<n)
+        {
+            int j=i+1;
+            while(j<n && chars[i]==chars[j])
+            {
+                j++;
+            }
+            
+            // yha kba aoge jb pura vector traverse krdiya 
+            // ya fir new character mila ho
+            
+            
+            
+            
+            // old character store krlo
+            
+            
+            chars[ansindex++]=chars[i];
+            int count=j-i;
+            
+            if(count>1)
+            {
+                string cnt=to_string(count);
+                
+                
+                for(char ch:cnt)
+                {
+                    chars[ansindex++]=ch;
+                }   
+                    
+            }
+            //moving to new different character
+            i=j;           
+             
+            
+        } 
+        return ansindex;
+        
+    }
+};
